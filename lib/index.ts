67bfdb2e45380e247adc6a48941e87ee83d649fd
Maxime GRANDCOLAS <maxime.grandcolas@scille.fr>
2024-04-26 15:47:17 +0200

    No default export for the plugin

--- lib/index.ts
+++ lib/index.ts
@@ -1,9 +1,6 @@
 export * from '@lib/common';
 export * from '@lib/components';
+export * from '@lib/plugin';
 export * from '@lib/services';
 export * from '@lib/states/darkMode';
 export * from '@lib/transitions';
-
-import { MegaSharkPlugin as MegaShark } from '@lib/plugin';
-
-export default MegaShark;
