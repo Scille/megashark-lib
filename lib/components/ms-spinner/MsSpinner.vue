bc039fc8734ea09e83d1355aa0985166e548fb72
fabienSvstr <fabien.sevestre@my-digital-school.org>
2024-05-02 12:14:17 +0200

    [MS] Change custom `MsSpinner`

--- lib/components/ms-spinner/MsSpinner.vue
+++ lib/components/ms-spinner/MsSpinner.vue
@@ -8,56 +8,88 @@
     >
       {{ $msTranslate(title) }}
     </ion-text>
-    <div class="container-spinner"></div>
+    <svg
+      :viewBox="`0 0 ${spinnerSize} ${spinnerSize}`"
+      class="circular-progress"
+    >
+      <circle class="bg"></circle>
+      <circle class="fg"></circle>
+    </svg>
   </div>
 </template>
 
 <script setup lang="ts">
 import { Translatable } from '@lib/services/translation';
 import { IonText } from '@ionic/vue';
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 
 const props = defineProps<{
   title?: Translatable;
   size?: number;
 }>();
 
-const spinnerSize = ref(`${props.size || 24}px`);
+const spinnerSize = ref(props.size || 24);
+const spinnerSizeFormatted = computed(() => `${spinnerSize.value}px`);
 </script>
 
 <style scoped lang="scss">
 .container {
   display: flex;
   align-items: center;
-  justify-content: center;
-  width: 100%;
-  overflow: hidden;
   gap: 0.5rem;
 
   &-text {
     color: var(--parsec-color-light-secondary-grey);
-    display: block;
-    width: 100%;
   }
+}
 
-  &-spinner {
-    position: relative;
-    width: v-bind(spinnerSize);
-    height: v-bind(spinnerSize);
-    border: calc(v-bind(spinnerSize) / 8) solid #cce2ff;
-    border-bottom-color: #4092ff;
-    border-radius: 50%;
-    display: inline-block;
-    box-sizing: border-box;
-    animation: rotation 1s linear infinite;
-    @keyframes rotation {
-      0% {
-        transform: rotate(0deg);
-      }
-      100% {
-        transform: rotate(360deg);
-      }
-    }
+.circular-progress {
+  width: v-bind(spinnerSizeFormatted);
+  height: v-bind(spinnerSizeFormatted);
+  --size: v-bind(spinnerSizeFormatted);
+  --half-size: calc(var(--size) / 2);
+  --stroke-width: calc(var(--size) / 8);
+  --radius: calc((var(--size) - var(--stroke-width)) / 2);
+  --circumference: calc(var(--radius) * pi * 2);
+  --dash: calc((30 * var(--circumference)) / 100);
+  animation: rotation 1s linear 0s infinite;
+}
+
+.circular-progress circle {
+  cx: var(--half-size);
+  cy: var(--half-size);
+  r: var(--radius);
+  stroke-width: var(--stroke-width);
+  fill: none;
+  stroke-linecap: round;
+}
+
+.circular-progress circle.bg {
+  stroke: var(--parsec-color-light-primary-50);
+}
+
+.circular-progress circle.fg {
+  transform: rotate(-90deg);
+  transform-origin: var(--half-size) var(--half-size);
+  stroke-dasharray: var(--dash) calc(var(--circumference) - var(--dash));
+  transition: stroke-dasharray 0.3s linear 0s;
+  stroke: var(--parsec-color-light-primary-500);
+}
+
+@property --progress {
+  syntax: '<number>';
+  inherits: false;
+  initial-value: 0;
+}
+
+@keyframes rotation {
+  from {
+    --progress: 0;
+    transform: rotate(0deg);
+  }
+  to {
+    --progress: 360;
+    transform: rotate(360deg);
   }
 }
 </style>
