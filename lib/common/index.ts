e272ed10442a32742cb468fff6cf9f165760696e
NicoTuxx <nicotuxxdev@gmail.com>
2024-05-15 18:59:39 +0200

    fix: replace Object.assign by deep merge

--- lib/common/index.ts
+++ lib/common/index.ts
@@ -2,4 +2,5 @@ export * from '@lib/common/asyncComputed';
 export * from '@lib/common/base64';
 export * from '@lib/common/clipboard';
 export * from '@lib/common/date';
+export * from '@lib/common/object';
 export * from '@lib/common/validation';
