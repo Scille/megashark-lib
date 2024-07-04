0586580abbbc86fa0ae774232229b7dc3e193bf7
NicoTuxx <nicotuxxdev@gmail.com>
2024-07-04 06:24:45 +0200

    feat: add defaultValue to Environment.get

--- lib/common/environment.ts
+++ lib/common/environment.ts
@@ -1,8 +1,8 @@
 // Parsec Cloud (https://parsec.cloud) Copyright (c) BUSL-1.1 2016-present Scille SAS
 
-function get(name: string): string | undefined {
+function get(name: string, defaultvalue?: string): string | undefined {
   const envName = name.startsWith('VITE_') ? name : `VITE_${name}`;
-  return import.meta.env[envName];
+  return import.meta.env[envName] ?? defaultvalue;
 }
 
 export enum EnvironmentType {
