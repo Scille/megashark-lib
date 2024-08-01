5991849394d2799d5b08dc5881c56bd2902aabec
Maxime GRANDCOLAS <maxime.grandcolas@gmail.com>
2024-08-01 10:35:48 +0200

    Renamed toogle to toggle

--- lib/components/ms-toggle/index.ts
+++ lib/components/ms-toggle/index.ts
@@ -1,6 +1,6 @@
 // Parsec Cloud (https://parsec.cloud) Copyright (c) BUSL-1.1 2016-present Scille SAS
-import MsBooleanToogle from '@lib/components/ms-toggle/MsBooleanToogle.vue';
+import MsBooleanToggle from '@lib/components/ms-toggle/MsBooleanToggle.vue';
 import MsGridListToggle from '@lib/components/ms-toggle/MsGridListToggle.vue';
 
 export * from '@lib/components/ms-toggle/types';
-export { MsBooleanToogle, MsGridListToggle };
+export { MsBooleanToggle, MsGridListToggle };
