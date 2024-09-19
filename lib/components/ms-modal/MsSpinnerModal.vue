2e505b0fa0d6dbeb12da72d2b9f28aa988d16819
Maxime GRANDCOLAS <maxime.grandcolas@scille.fr>
2024-09-19 15:58:43 +0200

    Fixed ms spinner modal

--- lib/components/ms-modal/MsSpinnerModal.vue
+++ lib/components/ms-modal/MsSpinnerModal.vue
@@ -2,7 +2,12 @@
 
 <template>
   <ms-modal :close-button="{ visible: false }">
-    <ion-text class="subtitles-normal spinner-label__text">{{ label }}</ion-text>
+    <ion-text
+      v-if="label"
+      class="subtitles-normal spinner-label__text"
+    >
+      {{ $msTranslate(label) }}
+    </ion-text>
     <ms-spinner />
   </ms-modal>
 </template>
@@ -11,6 +16,7 @@
 import MsModal from '@lib/components/ms-modal/MsModal.vue';
 import { Translatable } from '@lib/services';
 import MsSpinner from '@lib/components/ms-spinner/MsSpinner.vue';
+import { IonText } from '@ionic/vue';
 
 defineProps<{
   label?: Translatable;
