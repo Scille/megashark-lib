4637424899c031963475e97d62b3636d582e9a89
Maxime GRANDCOLAS <maxime.grandcolas@scille.fr>
2024-10-21 17:44:34 +0200

    [MS] Added date time picker

---
+++ lib/components/ms-datetime-picker/MsDatetimePicker.vue
@@ -0,0 +1,11 @@
+<!-- Parsec Cloud (https://parsec.cloud) Copyright (c) BUSL-1.1 2016-present Scille SAS -->
+
+<template>
+  <vue-date-picker v-bind="$attrs" />
+</template>
+
+<script setup lang="ts">
+import VueDatePicker from '@vuepic/vue-datepicker';
+</script>
+
+<style lang="scss" scoped></style>
---
+++ lib/components/ms-datetime-picker/index.ts
@@ -0,0 +1,5 @@
+// Parsec Cloud (https://parsec.cloud) Copyright (c) BUSL-1.1 2016-present Scille SAS
+
+import MsDatetimePicker from '@lib/components/ms-datetime-picker/MsDatetimePicker.vue';
+
+export { MsDatetimePicker };
