da9c21ed4f7e985dcc6a7b6a798d7a00982723bf
NicoTuxx <nicotuxxdev@gmail.com>
2024-11-06 18:20:18 +0100

    Update HomePage to include transitions

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -3,6 +3,7 @@
     <ion-content :fullscreen="true">
       <div id="container">
         <ion-title class="title-h1-xl main-title">{{ $msTranslate('usage.title') }}</ion-title>
+        <ion-title class="title-h1 main-title">{{ $msTranslate('usage.components.title') }}</ion-title>
         <!-- translation -->
         <div class="example-divider">
           <ion-label class="title-h2">{{ $msTranslate('usage.components.translation.title') }}</ion-label>
@@ -439,6 +440,37 @@
             </div>
           </div>
         </div>
+
+        <!-- transitions -->
+        <ion-title class="title-h1 main-title">{{ $msTranslate('usage.transitions.title') }}</ion-title>
+
+        <!-- slide-horizontal -->
+        <div class="example-divider">
+          <ion-label class="title-h2">{{ $msTranslate('usage.transitions.slideHorizontal.title') }}</ion-label>
+          <div class="example-divider-content">
+            <div class="example-data">
+              <ion-button @click="onPreviousItemSlideHorizontalClick">
+                {{ $msTranslate('usage.transitions.slideHorizontal.previousButton') }}
+              </ion-button>
+              <ion-button @click="onNextItemSlideHorizontalClick">
+                {{ $msTranslate('usage.transitions.slideHorizontal.nextButton') }}
+              </ion-button>
+            </div>
+          </div>
+          <div class="example-divider-content">
+            <div class="example-data">
+              <slide-horizontal
+                :appear-from="slideHorizontalPositions.appearFrom"
+                :disappear-to="slideHorizontalPositions.disappearTo"
+                class="slider-container"
+              >
+                <ion-label v-if="slideHorizontalItem === 0">{{ slideHorizontalItem }}</ion-label>
+                <ion-label v-if="slideHorizontalItem === 1">{{ slideHorizontalItem }}</ion-label>
+                <ion-label v-if="slideHorizontalItem === 2">{{ slideHorizontalItem }}</ion-label>
+              </slide-horizontal>
+            </div>
+          </div>
+        </div>
       </div>
     </ion-content>
   </ion-page>
@@ -506,6 +538,7 @@ import {
   openSpinnerModal as msOpenSpinnerModal,
   MsDatetimePicker,
 } from '@lib/components';
+import { Position, SlideHorizontal } from '@lib/transitions';
 import { DateTime } from 'luxon';
 import { inject, ref, Ref, onMounted } from 'vue';
 import SettingsModal from '@/views/settings/SettingsModal.vue';
@@ -586,6 +619,18 @@ const emailValidator: IValidator = async function (value: string) {
     ? { validity: Validity.Valid }
     : { validity: Validity.Invalid, reason: 'usage.components.inputs.msInput.invalidEmail' };
 };
+
+enum SlideHorizontalItem {
+  First,
+  Second,
+  Third,
+}
+
+const slideHorizontalItem = ref(SlideHorizontalItem.First);
+const slideHorizontalPositions = ref({
+  appearFrom: Position.Left,
+  disappearTo: Position.Right,
+});
 
 interface MsSorterExampleData {
   name: string;
@@ -715,6 +760,22 @@ async function createStripeCard(): Promise<void> {
   const result: PaymentMethodResult = await stripeCardForm.value.submit();
   stripeCardDetails.value = result?.paymentMethod?.card;
 }
+
+function onPreviousItemSlideHorizontalClick(): void {
+  slideHorizontalPositions.value = {
+    appearFrom: Position.Right,
+    disappearTo: Position.Left,
+  };
+  slideHorizontalItem.value = (slideHorizontalItem.value - 1 + 3) % 3;
+}
+
+function onNextItemSlideHorizontalClick(): void {
+  slideHorizontalPositions.value = {
+    appearFrom: Position.Left,
+    disappearTo: Position.Right,
+  };
+  slideHorizontalItem.value = (slideHorizontalItem.value + 1) % 3;
+}
 </script>
 
 <style scoped lang="scss">
@@ -790,5 +851,14 @@ async function createStripeCard(): Promise<void> {
 
 .stripe-card-form {
   width: 50em;
+}
+
+.slider-container ion-label {
+  width: 10em;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 5em;
+  background-color: red;
 }
 </style>
