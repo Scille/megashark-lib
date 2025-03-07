0f3acb1de320524b5dddd0cabbfcdcf11b033214
Maxime GRANDCOLAS <maxime.grandcolas@scille.fr>
2025-03-07 08:16:28 +0100

    [MS] Added missing export (oops)

--- lib/services/index.ts
+++ lib/services/index.ts
@@ -4,3 +4,4 @@ export * from '@lib/services/stripe';
 export * from '@lib/services/themeManager';
 export * from '@lib/services/toastManager';
 export * from '@lib/services/translation';
+export * from '@lib/services/windowSize';
