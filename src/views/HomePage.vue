5cdbad74016bdce633f1504b6060264f77699f61
NicoTuxx <nicolasstempert@gmail.com>
2025-03-12 14:24:33 +0100

    Add css mixin to manage responsive  - add responsive mixin  - update testing page to use responsive mixin  - harmonize size between windowSize & responsive mixin

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -578,26 +578,100 @@
           </div>
         </div>
 
-        <!-- window width -->
+        <!-- responsive -->
+        <ion-title class="title-h1 main-title">{{ $msTranslate('usage.responsive.title') }}</ion-title>
+
+        <!-- responsive mixin -->
         <div class="example-divider">
-          <ion-label class="title-h2">{{ $msTranslate('usage.windowWidth.title') }}</ion-label>
+          <ion-label class="title-h2">{{ $msTranslate('usage.responsive.mixin.title') }}</ion-label>
+          <div class="example-divider-content">
+            <ion-button @click="noResponsiveMixinOverlap = !noResponsiveMixinOverlap">
+              {{
+                noResponsiveMixinOverlap
+                  ? $msTranslate('usage.responsive.mixin.enableOverlap')
+                  : $msTranslate('usage.responsive.mixin.disableOverlap')
+              }}
+            </ion-button>
+          </div>
+          <div class="example-divider-content">
+            <ms-informative-text>
+              {{ $msTranslate('usage.responsive.mixin.overlapExplanation') }}
+            </ms-informative-text>
+          </div>
           <div class="example-divider-content">
             <div class="example-data">
-              <ion-label>{{ $msTranslate({ key: 'usage.windowWidth.label', data: { width: windowWidth } }) }}</ion-label>
-              <ion-label>{{ $msTranslate({ key: 'usage.windowWidth.largeWindow', data: { value: isLargeDisplay } }) }}</ion-label>
-              <ion-label>{{ $msTranslate({ key: 'usage.windowWidth.smallWindow', data: { value: isSmallDisplay } }) }}</ion-label>
+              <ion-label
+                class="responsive-mixin-block responsive-mixin-block-xs"
+                :class="{ 'no-overlap': noResponsiveMixinOverlap }"
+              >
+                {{ $msTranslate('usage.responsive.mixin.xs') }}
+              </ion-label>
+              <ion-label
+                class="responsive-mixin-block responsive-mixin-block-sm"
+                :class="{ 'no-overlap': noResponsiveMixinOverlap }"
+              >
+                {{ $msTranslate('usage.responsive.mixin.sm') }}
+              </ion-label>
+              <ion-label
+                class="responsive-mixin-block responsive-mixin-block-md"
+                :class="{ 'no-overlap': noResponsiveMixinOverlap }"
+              >
+                {{ $msTranslate('usage.responsive.mixin.md') }}
+              </ion-label>
+              <ion-label
+                class="responsive-mixin-block responsive-mixin-block-lg"
+                :class="{ 'no-overlap': noResponsiveMixinOverlap }"
+              >
+                {{ $msTranslate('usage.responsive.mixin.lg') }}
+              </ion-label>
+              <ion-label
+                class="responsive-mixin-block responsive-mixin-block-wide"
+                :class="{ 'no-overlap': noResponsiveMixinOverlap }"
+              >
+                {{ $msTranslate('usage.responsive.mixin.wide') }}
+              </ion-label>
+              <ion-label
+                class="responsive-mixin-block responsive-mixin-block-ultra-wide"
+                :class="{ 'no-overlap': noResponsiveMixinOverlap }"
+              >
+                {{ $msTranslate('usage.responsive.mixin.ultraWide') }}
+              </ion-label>
+              <ion-label
+                class="responsive-mixin-block responsive-mixin-block-no-mixin"
+                :class="{ 'no-overlap': noResponsiveMixinOverlap }"
+              >
+                {{ $msTranslate('usage.responsive.mixin.noMixin') }}
+              </ion-label>
+            </div>
+          </div>
+        </div>
+
+        <!-- window width -->
+        <div class="example-divider">
+          <ion-label class="title-h2">{{ $msTranslate('usage.responsive.windowWidth.title') }}</ion-label>
+          <div class="example-divider-content">
+            <div class="example-data">
+              <ion-label>
+                {{ $msTranslate({ key: 'usage.responsive.windowWidth.label', data: { width: windowWidth } }) }}
+              </ion-label>
+              <ion-label>
+                {{ $msTranslate({ key: 'usage.responsive.windowWidth.largeWindow', data: { value: isLargeDisplay } }) }}
+              </ion-label>
+              <ion-label>
+                {{ $msTranslate({ key: 'usage.responsive.windowWidth.smallWindow', data: { value: isSmallDisplay } }) }}
+              </ion-label>
             </div>
             <div
               class="example-data"
               v-show="isLargeDisplay"
             >
-              {{ $msTranslate('usage.windowWidth.shownOnLargeDisplays') }}
+              {{ $msTranslate('usage.responsive.windowWidth.shownOnLargeDisplays') }}
             </div>
             <div
               class="example-data"
               v-show="isSmallDisplay"
             >
-              {{ $msTranslate('usage.windowWidth.shownOnSmallDisplays') }}
+              {{ $msTranslate('usage.responsive.windowWidth.shownOnSmallDisplays') }}
             </div>
           </div>
         </div>
@@ -748,6 +822,7 @@ const b64EncodedResult = ref('');
 const enableDragging = ref(true);
 const restrictDirection = ref({ up: false, down: false, left: false, right: false });
 const draggableElement = ref();
+const noResponsiveMixinOverlap = ref(false);
 
 const msSorterOptions: MsOptions = new MsOptions([
   { label: 'usage.components.sorter.name', key: 'name' },
@@ -1047,5 +1122,84 @@ function onSliderPlayClicked(): void {
   align-items: center;
   height: 5em;
   background-color: red;
+}
+
+@import '@lib/theme/variables/responsive-mixin';
+
+.responsive-mixin-block {
+  display: none;
+  padding: .5em;
+  border-radius: .5em;
+  background: blue;
+  &-no-mixin {
+    display: block;
+    color: white;
+  }
+
+  @include breakpoint('ultra-wide') {
+    background: #ff0000;
+    &-ultra-wide {
+      display: block;
+    }
+
+    &-no-mixin.no-overlap {
+      display: none;
+    }
+  }
+
+  @include breakpoint('wide') {
+    background: #ff5500;
+    &-wide {
+      display: block;
+    }
+
+    &-ultra-wide.no-overlap {
+      display: none;
+    }
+  }
+
+  @include breakpoint('lg') {
+    background: #ffaa00;
+    &-lg {
+      display: block;
+    }
+
+    &-wide.no-overlap {
+      display: none;
+    }
+  }
+
+  @include breakpoint('md') {
+    background: #aaff00;
+    &-md {
+      display: block;
+    }
+
+    &-lg.no-overlap {
+      display: none;
+    }
+  }
+
+  @include breakpoint('sm') {
+    background: #55ff00;
+    &-sm {
+      display: block;
+    }
+
+    &-md.no-overlap {
+      display: none;
+    }
+  }
+
+  @include breakpoint('xs') {
+    background: #00ff00;
+    &-xs {
+      display: block;
+    }
+
+    &-sm.no-overlap {
+      display: none;
+    }
+  }
 }
 </style>
