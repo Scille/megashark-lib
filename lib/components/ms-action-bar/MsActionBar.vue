af0ac9519475f9c6f9bd94668740a871fff5c513
fabienSvstr <fabien.sevestre@my-digital-school.org>
2025-06-04 14:43:24 +0200

    [MS] Added "More button" to action bar depending on its current size

--- lib/components/ms-action-bar/MsActionBar.vue
+++ lib/components/ms-action-bar/MsActionBar.vue
@@ -2,17 +2,48 @@
 
 <template>
   <ion-item-divider class="toolbar ion-margin-bottom secondary">
-    <div class="action-bar">
-      <div class="action-bar-buttons-list">
-        <ms-action-bar-button
-          v-for="button in buttons"
-          :key="button.icon"
-          :button-label="button.label"
-          :icon="button.icon"
-          :image="button.image"
-          :is-dropdown="button.isDropdown"
-          @click="button.onClick"
-        />
+    <div
+      class="action-bar"
+      ref="actionBarRef"
+    >
+      <div class="action-bar-buttons-content">
+        <div
+          class="action-bar-buttons-list"
+          ref="actionBarButtonsListRef"
+        >
+          <ms-action-bar-button
+            v-for="(button, index) in buttons"
+            :key="button.icon || index"
+            :button-label="button.label"
+            :icon="button.icon"
+            :image="button.image"
+            :is-dropdown="button.isDropdown"
+            @click="button.onClick"
+            :ref="(button) => setButtonRef(button, index)"
+            :class="{
+              'ms-action-bar-button--visible': buttonVisibilityStates[index],
+              'ms-action-bar-button--hidden': !buttonVisibilityStates[index],
+            }"
+          />
+          <ion-button
+            id="action-bar-more-button"
+            class="button-medium"
+            @click="openActionBarPopover($event)"
+            v-show="hiddenButtons.length > 0"
+            ref="actionBarMoreButtonRef"
+          >
+            <ion-icon
+              slot="icon-only"
+              class="more-icon__ellipsis"
+              :icon="ellipsisHorizontal"
+            />
+            <ion-icon
+              class="more-icon__chevron"
+              slot="end"
+              :icon="chevronDown"
+            />
+          </ion-button>
+        </div>
       </div>
       <slot />
     </div>
@@ -20,11 +51,14 @@
 </template>
 
 <script setup lang="ts">
-import { IonItemDivider } from '@ionic/vue';
+import { IonItemDivider, popoverController, IonButton, IonIcon } from '@ionic/vue';
 import { MsActionBarButton } from '@lib/components';
 import { Translatable } from '@lib/services';
+import { chevronDown, ellipsisHorizontal } from 'ionicons/icons';
+import { ref, onMounted, nextTick, watch, computed, onUnmounted } from 'vue';
+import MsActionBarPopover from '@lib/components/ms-action-bar/MsActionBarPopover.vue';
 
-defineProps<{
+const props = defineProps<{
   buttons: {
     label: Translatable;
     icon?: string;
@@ -33,6 +67,180 @@ defineProps<{
     onClick?: (event: MouseEvent) => Promise<void>;
   }[];
 }>();
+
+const buttonComponentRefs = ref<any[]>([]);
+const actionBarRef = ref<HTMLElement>();
+const actionBarButtonsListRef = ref<HTMLElement>();
+const buttonVisibilityStates = ref<boolean[]>([]);
+const actionBarWidth = ref(0);
+let resizeObserver: ResizeObserver | null = null;
+
+// Returns the list of hidden buttons
+const hiddenButtons = computed(() => {
+  const hidden = props.buttons.filter((_, index) => buttonVisibilityStates.value[index] === false);
+  return hidden;
+});
+
+/*
+ * Calculates which buttons should be visible based on available space.
+ * - Shows all buttons temporarily to measure their actual width
+ * - Determines how many buttons can fit in the available space
+ * - Reserves space for the "more" button if some buttons need to be hidden
+ * - Updates buttonVisibilityStates to toggle button visibility
+ */
+async function calculateButtonVisibility(): Promise<void> {
+  if (!actionBarRef.value || actionBarWidth.value === 0) {
+    return;
+  }
+
+  buttonVisibilityStates.value = new Array(props.buttons.length).fill(true);
+  await nextTick();
+
+  const availableWidth = getButtonListWidth();
+  const MORE_BUTTON_WIDTH = 32;
+
+  if (availableWidth < MORE_BUTTON_WIDTH) {
+    buttonVisibilityStates.value = new Array(props.buttons.length).fill(false);
+    return;
+  }
+
+  let totalWidth = 0;
+  const newVisibility: boolean[] = [];
+
+  for (let i = 0; i < props.buttons.length; i++) {
+    const buttonWidth = getButtonWidth(i);
+    if (buttonWidth === 0) {
+      newVisibility[i] = false;
+      continue;
+    }
+
+    const gap = i > 0 ? 8 : 0;
+    const widthNeeded = totalWidth + buttonWidth + gap;
+    const isLastButton = i === props.buttons.length - 1;
+
+    const maxAllowedWidth = isLastButton ? availableWidth : availableWidth - MORE_BUTTON_WIDTH;
+
+    if (widthNeeded <= maxAllowedWidth) {
+      newVisibility[i] = true;
+      totalWidth = widthNeeded;
+    } else {
+      // This button and all subsequent ones will be hidden
+      newVisibility.push(...new Array(props.buttons.length - i).fill(false));
+      break;
+    }
+  }
+
+  buttonVisibilityStates.value = newVisibility;
+}
+
+// Returns the available width for displaying buttons (excluding slot)
+function getButtonListWidth(): number {
+  const buttonListRect = actionBarButtonsListRef.value!.getBoundingClientRect();
+  const slotWidth = actionBarWidth.value - buttonListRect.width;
+  return actionBarWidth.value - slotWidth;
+}
+
+// Returns the width of a button
+function getButtonWidth(index: number): number {
+  const buttonComponent = buttonComponentRefs.value[index];
+  const buttonElement = buttonComponent?.actionBarButtonRef?.value || buttonComponent?.$el;
+  return buttonElement ? buttonElement.getBoundingClientRect().width : 0;
+}
+
+// Watches for changes in the number of buttons
+// When buttons are added or removed, recalculates which ones can be displayed
+watch(
+  () => props.buttons.length,
+  () => {
+    nextTick(() => calculateButtonVisibility());
+  },
+  { immediate: true },
+);
+
+// Watches for changes in the action bar width
+// Recalculates button visibility when the available space changes
+watch(
+  () => actionBarWidth.value,
+  (newWidth) => {
+    if (newWidth > 0) {
+      calculateButtonVisibility();
+    }
+  },
+);
+
+// ResizeObserver to monitor action bar width changes
+function setupResizeObserver(): void {
+  if (!actionBarRef.value) return;
+
+  resizeObserver = new ResizeObserver((entries) => {
+    for (const entry of entries) {
+      const newWidth = entry.contentRect.width;
+      if (newWidth !== actionBarWidth.value) {
+        actionBarWidth.value = newWidth;
+      }
+    }
+  });
+
+  resizeObserver.observe(actionBarRef.value);
+}
+
+// Allows to expose a method to recalculate button visibility
+function recalculateVisibility(): void {
+  if (actionBarRef.value) {
+    const rect = actionBarRef.value.getBoundingClientRect();
+    actionBarWidth.value = rect.width;
+  }
+
+  setTimeout(() => {
+    calculateButtonVisibility();
+  }, 10);
+}
+
+// Sets the ref for each button
+function setButtonRef(button: any, index: number): void {
+  if (button) {
+    buttonComponentRefs.value[index] = button;
+  }
+}
+
+async function openActionBarPopover(event: Event): Promise<void> {
+  const popover = await popoverController.create({
+    component: MsActionBarPopover,
+    cssClass: 'action-bar-more-popover',
+    componentProps: {
+      hiddenButtons: hiddenButtons.value,
+    },
+    event: event,
+    alignment: 'end',
+    showBackdrop: false,
+  });
+  await popover.present();
+  await popover.onDidDismiss();
+}
+
+onMounted(async () => {
+  await nextTick();
+
+  // Configuration initiale
+  setupResizeObserver();
+
+  if (actionBarRef.value) {
+    const rect = actionBarRef.value.getBoundingClientRect();
+    actionBarWidth.value = rect.width;
+  }
+
+  calculateButtonVisibility();
+});
+
+onUnmounted(() => {
+  if (resizeObserver) {
+    resizeObserver.disconnect();
+  }
+});
+
+defineExpose({
+  recalculateVisibility,
+});
 </script>
 
 <style scoped lang="scss">
@@ -49,9 +257,59 @@ defineProps<{
   box-shadow: none;
 }
 
+.action-bar {
+  display: flex;
+  gap: 0.5rem;
+}
+
+#action-bar-more-button {
+  --background: transparent;
+  --background-hover: var(--parsec-color-light-secondary-medium);
+  --color: var(--parsec-color-light-secondary-text);
+  margin-left: 0.5rem;
+  min-height: 1rem;
+
+  &::part(native) {
+    padding: 0.25rem 0.5rem;
+  }
+
+  .more-icon__ellipsis {
+    font-size: 1.125rem;
+    color: var(--parsec-color-light-secondary-soft-text);
+  }
+
+  .more-icon__chevron {
+    margin-inline: 0em;
+    margin-left: 0.375rem;
+    font-size: 1rem;
+    color: var(--parsec-color-light-secondary-soft-text);
+  }
+
+  &:hover {
+    .more-icon__chevron,
+    .more-icon__ellipsis {
+      color: var(--parsec-color-light-primary-600);
+    }
+  }
+}
+
+.action-bar-buttons-content {
+  display: flex;
+  width: 100%;
+}
+
 .action-bar-buttons-list {
+  width: calc(100% - 2rem);
   display: flex;
   align-items: center;
   gap: 0.5rem;
+}
+
+.ms-action-bar-button--visible {
+  display: flex;
+}
+
+.ms-action-bar-button--hidden {
+  display: none;
 }
 </style>
