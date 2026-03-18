b5762b4491a15ad65d9b705256e7fd665276b6b3
Marcos Medrano <marcosmedrano0@gmail.com>
2026-03-18 15:21:13 +0100

    Fix VueDatePicker import from @vuepic/vue-datepicker
    See:
    https://vue3datepicker.com/migration/from-v11#importing-the-component

--- lib/components/ms-datetime-picker/MsDatetimePicker.vue
+++ lib/components/ms-datetime-picker/MsDatetimePicker.vue
@@ -8,7 +8,7 @@
 </template>
 
 <script setup lang="ts">
-import VueDatePicker from '@vuepic/vue-datepicker';
+import { VueDatePicker } from '@vuepic/vue-datepicker';
 </script>
 
 <style lang="scss" scoped></style>
