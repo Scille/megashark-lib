7db92c4413c4d983498f73709d44b7d46238c250
Marcos Medrano <marcosmedrano0@gmail.com>
2026-04-14 17:21:00 +0200

    Add lintignore for enum members reported by knip

--- lib/components/ms-progress/types.ts
+++ lib/components/ms-progress/types.ts
@@ -1,5 +1,7 @@
 export enum MsProgressAppearance {
   Bar = 'bar',
+  /** @lintignore */ // TODO: knip reports it as unused enum member, why?
   Line = 'line',
+  /** @lintignore */ // TODO: knip reports it as unused enum member, why?
   ProgressBar = 'progress-bar',
 }
