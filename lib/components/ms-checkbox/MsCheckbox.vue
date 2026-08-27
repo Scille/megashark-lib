7a55c21a4f23ce91aeb22a937b84da161aba864c
fabienSvstr <fabien.sevestre@scille.fr>
2026-08-27 13:39:11 +0200

    update(component): `MsCheckbox`

--- lib/components/ms-checkbox/MsCheckbox.vue
+++ lib/components/ms-checkbox/MsCheckbox.vue
@@ -21,7 +21,7 @@
     />
     <div
       v-if="$slots.default"
-      class="button-medium ms-checkbox-text"
+      class="ms-checkbox-text"
     >
       <slot />
     </div>
@@ -100,11 +100,77 @@ async function onChange(_event: Event): Promise<void> {
 </script>
 
 <style lang="scss" scoped>
+@use '@lib/theme' as ms;
+
+.ms-checkbox {
+  appearance: none;
+  -webkit-appearance: none;
+  width: calc(1.125 * ms.size('xs'));
+  min-width: calc(1.125 * ms.size('xs'));
+  height: calc(1.125 * ms.size('xs'));
+  min-height: calc(1.125 * ms.size('xs'));
+  border-radius: ms.radius('sm');
+  border: ms.border('thin') solid ms.color('border-base-default');
+  background: ms.color('surface-base-default');
+  cursor: pointer;
+  position: relative;
+  place-content: center;
+  box-shadow: ms.shadow('light');
+  transition:
+    background-color 0.2s ease,
+    border-color 0.2s ease,
+    transform 0.12s ease,
+    box-shadow 0.12s linear;
+
+  &:active {
+    transform: scale(0.9);
+  }
+
+  &::before {
+    content: '';
+    position: absolute;
+    width: 4px;
+    height: 1.5px;
+    background: ms.color('surface-base-default');
+    opacity: 0;
+    border-radius: 1px;
+    transform: rotate(0deg) scale(1);
+    top: calc(50% - 0.5px);
+    right: calc(50% - 1px);
+    transition:
+      transform 150ms ease,
+      opacity 150ms ease;
+  }
+
+  &::after {
+    content: '';
+    position: absolute;
+    width: 8px;
+    height: 1.25px;
+    background: ms.color('surface-base-default');
+    opacity: 0;
+    border-radius: 1px;
+    transform: rotate(0deg) scale(1);
+    top: calc(50% - 0.5px);
+    right: calc(50% - 4px);
+    transition:
+      transform 0.18s ease,
+      opacity 0.12s ease;
+  }
+
+  &-text {
+    @include ms.font('label-md-medium');
+    user-select: none;
+    cursor: pointer;
+    color: ms.color('text-base-label');
+  }
+}
+
 .checkbox-container {
   display: flex;
   align-items: center;
-  gap: 0.5em;
-  padding: 0.25rem 0;
+  gap: ms.spacing('gap-lg');
+  padding: ms.spacing('padding-sm') ms.spacing('padding-none');
   cursor: pointer;
 
   &-right {
@@ -116,100 +182,48 @@ async function onChange(_event: Event): Promise<void> {
     flex-direction: row-reverse;
   }
 
-  &:hover {
-    .ms-checkbox {
-      outline: 1px solid var(--parsec-color-primary-300);
-      outline-offset: 2px;
+  &:hover .ms-checkbox {
+    background: ms.color('surface-neutral-default-subtle-hover');
+
+    &:checked,
+    &[ms-indeterminate='true'] {
+      background: ms.color('surface-brand-default-hover');
+      border-color: ms.color('border-brand-default-hover');
+    }
+
+    &-text {
+      color: ms.color('text-neutral-default-hover');
     }
   }
 }
 
-.ms-checkbox {
-  appearance: none;
-  -webkit-appearance: none;
-  width: 20px;
-  min-width: 20px;
-  height: 20px;
-  min-height: 20px;
-  border-radius: var(--parsec-radius-6);
-  border: 2px solid var(--parsec-color-secondary-light);
-  background: var(--parsec-color-secondary-surface);
-  cursor: pointer;
-  position: relative;
-  display: grid;
-  place-content: center;
-  outline: 1px solid transparent;
-  transition:
-    background-color 0.2s ease,
-    border-color 0.2s ease,
-    transform 0.12s ease,
-    outline 0.12s linear;
-
-  &:active {
-    transform: scale(0.9);
-  }
-
-  &::before {
-    content: '';
-    position: absolute;
-    width: 6px;
-    height: 2px;
-    background: var(--parsec-color-secondary-surface);
-    opacity: 0;
-    border-radius: 1px;
-    transform: rotate(0deg) scale(1);
-    top: calc(50% - 1px);
-    right: calc(50% - 0.5px);
-    transition:
-      transform 0.18s ease,
-      opacity 0.18s ease;
-  }
-
-  &::after {
-    content: '';
-    position: absolute;
-    width: 10px;
-    height: 2px;
-    background: var(--parsec-color-secondary-surface);
-    opacity: 0;
-    border-radius: 1px;
-    transform: rotate(0deg) scale(1);
-    top: calc(50% - 1px);
-    right: calc(50% - 6px);
-    transition:
-      transform 0.18s ease,
-      opacity 0.12s ease;
-  }
-
-  &-text {
-    user-select: none;
-    cursor: pointer;
-    color: var(--parsec-color-secondary-soft-text);
-  }
+.ms-checkbox:focus-visible {
+  border-color: ms.color('border-brand-default');
+  box-shadow: ms.shadow('focus-brand');
 }
 
 .ms-checkbox:checked {
-  background: var(--parsec-color-primary-600);
-  border-color: var(--parsec-color-primary-600);
+  background: ms.color('surface-brand-default');
+  border-color: ms.color('border-brand-default');
 }
 
 .ms-checkbox:checked::before {
-  transform: rotate(45deg) scale(1);
+  transform: rotate(45deg) scale(1.25);
   opacity: 1;
   top: 9px;
   right: 8px;
 }
 
 .ms-checkbox:checked::after {
-  transform: rotate(-50deg) scale(1);
+  transform: rotate(-50deg) scale(1.25);
   opacity: 1;
   top: 7px;
-  right: 1.5px;
+  right: 2.3px;
 }
 
 .ms-checkbox[ms-indeterminate='true'] {
-  background: var(--parsec-color-primary-600);
-  border-color: var(--parsec-color-primary-600);
+  background: ms.color('surface-brand-default');
+  border-color: ms.color('border-brand-default');
 
   &::before {
     opacity: 1;
